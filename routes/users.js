757e4f887e0498ac69152aa3d74d5d8fa0c6c535
wdf <wdf332@gmail.com>
2023-06-01 23:30:00 +0800

    edit

--- routes/users.js
+++ routes/users.js
@@ -31,8 +31,10 @@ router.patch('/extend', auth.jwt, extend)
 router.get('/me', auth.jwt, getUser)
 
 // ------------------admin------------------
-router.get('/all', auth.jwt, admin, getAllUser) // 獲取全部使用者
+// 獲取全部使用者
+router.get('/all', auth.jwt, admin, getAllUser)
 router.delete('/delete/:id', auth.jwt, admin, deleteUser)
+// -------------------------------------------
 router.get('/allvacation', auth.jwt, findAllUserVacation)
 router.get('/vacation/:name', auth.jwt, findUserVacation)
 
