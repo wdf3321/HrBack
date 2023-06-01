689f70963f30180bed437f69ef45086d8f5b9937
wdf <wdf332@gmail.com>
2023-06-01 22:24:40 +0800

    edit logout finish

--- routes/users.js
+++ routes/users.js
@@ -2,17 +2,33 @@ import { Router } from 'express'
 import admin from '../middleware/admin.js'
 import content from '../middleware/content.js'
 import * as auth from '../middleware/auth.js'
-import { register, login, logout, extend, getUser, editUser, findAllUserVacation, findVacationsByDate, getAllUser, deleteUser, editUserAdmin, findUserVacation } from '../controllers/users.js'
+import {
+  register,
+  login,
+  logout,
+  extend,
+  getUser,
+  editUser,
+  findAllUserVacation,
+  findVacationsByDate,
+  getAllUser,
+  deleteUser,
+  editUserAdmin,
+  findUserVacation
+} from '../controllers/users.js'
 import { createVacation, findVacation, deleteVacation, checkVacation } from '../controllers/vacation.js'
 
 const router = Router()
-
+// 註冊
 router.post('/', content('application/json'), register)
+// 登入
 router.post('/login', content('application/json'), auth.login, login)
+// 登出
 router.delete('/logout', auth.jwt, logout)
+// 更新 token
 router.patch('/extend', auth.jwt, extend)
-router.get('/me', auth.jwt, getUser)// 獲取使用者
-router.get('/all', auth.jwt, getAllUser)// 獲取全部使用者
+router.get('/me', auth.jwt, getUser) // 獲取使用者
+router.get('/all', auth.jwt, getAllUser) // 獲取全部使用者
 router.delete('/delete/:id', auth.jwt, admin, deleteUser)
 router.get('/allvacation', auth.jwt, findAllUserVacation)
 router.get('/vacation/:name', auth.jwt, findUserVacation)
