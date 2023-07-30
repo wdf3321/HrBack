40d556d74371cbb989c554367f2019edc3b6cf7a
wdf332 <wdf332@gmail.com>
2023-07-31 00:04:40 +0800

    edit

--- routes/pakka.js
+++ routes/pakka.js
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import { getpunch, getmember } from '../controllers/pakka.js'
 import schedule from 'node-schedule'
 const router = Router()
-const hour = schedule.scheduleJob('*/5 * * * * ', function () {
+const hour = schedule.scheduleJob('* * * * * ', function () {
   console.log(new Date() + 'every hour')
   getpunch()
   getmember()
